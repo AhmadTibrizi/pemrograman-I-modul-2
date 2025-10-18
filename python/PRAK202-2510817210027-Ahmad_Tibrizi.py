1afc213cb3e4f7b3a3c4f612a5b5babd056084a6
Riez69 <ahmadbarangaja12@gmail.com>
2025-10-18 11:16:37 +0800

    3

---
+++ python/PRAK202-2510817210027-Ahmad_Tibrizi.py
@@ -0,0 +1,5 @@
+a = float(input("Masukkan Nilai Pertama: "))
+b = float(input("Masukkan Nilai Kedua: "))
+
+hasil = a + b
+print(f'Hasil dari penjumlahan nilai pertama "{a}" dan nilai kedua "{b}" adalah "{hasil:.2f}"')
---
+++ python/PRAK203-2510817210027-Ahmad_Tibrizi.py
@@ -0,0 +1,9 @@
+a = float(input("Masukkan a: "))
+b = float(input("Masukkan b: "))
+i = float(input("Masukkan i: "))
+j = float(input("Masukkan j: "))
+x = float(input("Masukkan x: "))
+y = float(input("Masukkan y: "))
+
+hasil = ((a - b) * (i / j)) - x - y
+print(f"Hasil = {hasil:.3f}")
