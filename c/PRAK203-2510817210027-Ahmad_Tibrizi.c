f2bc6ef6d9a29cc4628c65e8d8a7611863d9ac82
Riez69 <ahmadbarangaja12@gmail.com>
2025-10-18 13:28:29 +0800

    3 .

---
+++ c/PRAK203-2510817210027-Ahmad_Tibrizi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, i, j, x, y, hasil;
+
+    printf("Masukkan a: ");
+    scanf("%f", &a);
+    printf("Masukkan b: ");
+    scanf("%f", &b);
+    printf("Masukkan i: ");
+    scanf("%f", &i);
+    printf("Masukkan j: ");
+    scanf("%f", &j);
+    printf("Masukkan x: ");
+    scanf("%f", &x);
+    printf("Masukkan y: ");
+    scanf("%f", &y);
+
+    hasil = ((a - b) * (i / j)) - x - y;
+    printf("Hasil = %.3f\n", hasil);
+    return 0;
+}
